fdd4d63ed4e39a0399e0cedd3603774978931883
Gregory M. Turner <gmt@be-evil.net>
2014-10-12 12:37:12 -0700

    Fix up all whitespace trouble (hopefully)
    Signed-off-by: Gregory M. Turner <gmt@be-evil.net>

--- lookmanosources/configs.py
+++ lookmanosources/configs.py
@@ -35,71 +35,67 @@ import os
 import string
 
 try: # py2
-	letters = string.letters
+    letters = string.letters
 except AttributeError: # py3
-	letters = string.ascii_letters
+    letters = string.ascii_letters
 
 def get_kernel_ng_conf_path(EPREFIX):
-	return EPREFIX + '/etc/kernel-ng.conf'
+    return EPREFIX + '/etc/kernel-ng.conf'
 
 #def write_make_conf(output, config_path, var, mirror_string):
-#	"""Write the make.conf target changes
+#    """Write the make.conf target changes
 #
-#	@param output: file, or output to print messages to
-#	@param mirror_string: "var='hosts'" string to write
-#	@param config_path; string
-#	"""
-#	output.write('\n')
-#	output.print_info('Modifying %s with new mirrors...\n' % config_path)
-#	try:
-#		config = open(config_path, 'r')
-#		output.write('\tReading make.conf\n')
-#		lines = config.readlines()
-#		config.close()
-#		output.write('\tMoving to %s.backup\n' % config_path)
-#		shutil.move(config_path, config_path + '.backup')
-#	except IOError:
-#		lines = []
+#    @param output: file, or output to print messages to
+#    @param mirror_string: "var='hosts'" string to write
+#    @param config_path; string
+#    """
+#    output.write('\n')
+#    output.print_info('Modifying %s with new mirrors...\n' % config_path)
+#    try:
+#        config = open(config_path, 'r')
+#        output.write('\tReading make.conf\n')
+#        lines = config.readlines()
+#        config.close()
+#        output.write('\tMoving to %s.backup\n' % config_path)
+#        shutil.move(config_path, config_path + '.backup')
+#    except IOError:
+#        lines = []
 #
-#	regex = re.compile('^%s=.*' % var)
-#	for line in lines:
-#		if regex.match(line):
-#			lines.remove(line)
+#    regex = re.compile('^%s=.*' % var)
+#    for line in lines:
+#        if regex.match(line):
+#            lines.remove(line)
 #
-#	lines.append(mirror_string)
+#    lines.append(mirror_string)
+#    output.write('\tWriting new %s\n' % config_path)
+#    config = open(config_path, 'w')
 #
-#	output.write('\tWriting new %s\n' % config_path)
+#    for line in lines:
+#        config.write(line)
+#    config.write('\n')
+#    config.close()
 #
-#	config = open(config_path, 'w')
-#
-#	for line in lines:
-#		config.write(line)
-#	config.write('\n')
-#	config.close()
-#
-#	output.print_info('Done.\n')
-#	sys.exit(0)
-#
+#    output.print_info('Done.\n')
+#    sys.exit(0)
 #
 #def write_repos_conf(output, config_path, var, value):
-#	"""Saves the new var value to a ConfigParser style file
+#    """Saves the new var value to a ConfigParser style file
 #
-#	@param output: file, or output to print messages to
-#	@param config_path; string
-#	@param var: string; the variable to save teh value to.
-#	@param value: string, the value to set var to
-#	"""
-#	try:
-#		from configparser import ConfigParser
-#	except ImportError:
-#		from ConfigParser import ConfigParser
-#	config = ConfigParser()
-#	config.read(config_path)
-#	if config.has_option('gentoo', var):
-#		config.set('gentoo', var, value)
-#		with open(config_path, 'w') as configfile:
-#			config.write(configfile)
-#	else:
-#		output.print_err("write_repos_conf(): Failed to find section 'gentoo',"
-#			" variable: %s\nChanges NOT SAVED" %var)
-
+#    @param output: file, or output to print messages to
+#    @param config_path; string
+#    @param var: string; the variable to save teh value to.
+#    @param value: string, the value to set var to
+#    """
+#    try:
+#        from configparser import ConfigParser
+#    except ImportError:
+#        from ConfigParser import ConfigParser
+#    config = ConfigParser()
+#    config.read(config_path)
+#    if config.has_option('gentoo', var):
+#        config.set('gentoo', var, value)
+#        with open(config_path, 'w') as configfile:
+#            config.write(configfile)
+#    else:
+#        output.print_err("write_repos_conf(): Failed to find section 'gentoo',"
+#            " variable: %s\nChanges NOT SAVED" %var)
--- lookmanosources/main.py
+++ lookmanosources/main.py
@@ -42,9 +42,9 @@ from lookmanosources.version import version
 
 # eprefix compatibility
 try:
-	from portage.const import rootuid
+    from portage.const import rootuid
 except ImportError:
-	rootuid = 0
+    rootuid = 0
 
 # establish the eprefix, initially set so eprefixify can
 # set it on install
@@ -55,175 +55,174 @@ if "GENTOO_PORTAGE_EPREFIX" in EPREFIX:
     EPREFIX = ''
 
 class LookMaNoSources(object):
-	'''Main operational class'''
+    '''Main operational class'''
 
-	def __init__(self, output=None):
-		'''LookMaNoSources class init
+    def __init__(self, output=None):
+        '''LookMaNoSources class init
 
-		@param output: lookmanosources.output.Ouptut() class instance
-			or None for the default instance
-		'''
-		self.output = output or Output()
+        @param output: lookmanosources.output.Ouptut() class instance
+        or None for the default instance
+        '''
+        self.output = output or Output()
 
-	@staticmethod
-	def _have_bin(name):
-		"""Determines whether a particular binary is available
-		on the host system.  It searches in the PATH environment
-		variable paths.
+    @staticmethod
+    def _have_bin(name):
+        """Determines whether a particular binary is available
+        on the host system.  It searches in the PATH environment
+        variable paths.
 
-		@param name: string, binary name to search for
-		@rtype: string or None
-		"""
-		for path_dir in os.environ.get("PATH", "").split(":"):
-			if not path_dir:
-				continue
-			file_path = os.path.join(path_dir, name)
-			if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
-				return file_path
-		return None
+        @param name: string, binary name to search for
+        @rtype: string or None
+        """
+        for path_dir in os.environ.get("PATH", "").split(":"):
+            if not path_dir:
+                continue
+            file_path = os.path.join(path_dir, name)
+            if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+                return file_path
+        return None
 
-	def change_config(self, features, out, config_path, sync=False):
-		"""Writes the config changes to the given file, or to stdout.
+    def change_config(self, features, out, config_path, sync=False):
+        """Writes the config changes to the given file, or to stdout.
 
-		@param features: list of host urls to write
-		@param out: boolean, used to redirect output to stdout
-		@param config_path; string
-		"""
-		if hasattr(features[0], 'decode'):
-			features = [x.decode('utf-8') for x in features]
+        @param features: list of host urls to write
+        @param out: boolean, used to redirect output to stdout
+        @param config_path; string
+        """
+        if hasattr(features[0], 'decode'):
+            features = [x.decode('utf-8') for x in features]
+        feature_string = 'features = "%s"' % ' '.join(features)
 
-		feature_string = 'features = "%s"' % ' '.join(features)
+        if out:
+            self.write_to_output(feature_string)
+        else:
+            write_kernel_ng_conf(self.output, config_path,
+                'features', feature_string)
 
-		if out:
-			self.write_to_output(feature_string)
-		else:
-			write_kernel_ng_conf(self.output, config_path,
-                                'features', feature_string)
+    @staticmethod
+    def write_to_output(feature_string):
+        print()
+        print(feature_string)
+        sys.exit(0)
 
-	@staticmethod
-	def write_to_output(feature_string):
-		print()
-		print(feature_string)
-		sys.exit(0)
+    def _parse_args(self, argv, config_path):
+        """
+        Does argument parsing and some sanity checks.
+        Returns an optparse Options object.
 
-	def _parse_args(self, argv, config_path):
-		"""
-		Does argument parsing and some sanity checks.
-		Returns an optparse Options object.
+        The descriptions, grouping, and possibly the amount sanity checking
+        need some finishing touches.
+        """
+        desc = "\n".join((
+            self.output.white("examples:"),
+            "",
+            self.output.white("	 automatic:"),
+            "		 # look-ma-no-sources -foo",
+            "		 # look-ma-no-sources --bar --baz >> /mnt/gentoo/etc/kernel-ng/kernel-ng.conf",
+            "",
+            self.output.white("	 interactive:"),
+            "		 # look-ma-no-sources -i",
+            ))
+        parser = OptionParser(
+            formatter=ColoredFormatter(self.output), description=desc,
+            version='Look, Ma!  No sources!  version: %s' % version)
 
-		The descriptions, grouping, and possibly the amount sanity checking
-		need some finishing touches.
-		"""
-		desc = "\n".join((
-			self.output.white("examples:"),
-			"",
-			self.output.white("	 automatic:"),
-			"		 # look-ma-no-sources -foo",
-			"		 # look-ma-no-sources --bar --baz >> /mnt/gentoo/etc/kernel-ng/kernel-ng.conf",
-			"",
-			self.output.white("	 interactive:"),
-			"		 # look-ma-no-sources -i",
-			))
-		parser = OptionParser(
-			formatter=ColoredFormatter(self.output), description=desc,
-			version='Look, Ma!  No sources!  version: %s' % version)
+        group = parser.add_option_group("Main modes")
+        group.add_option(
+            "-i", "--interactive", action="store_true", default=False,
+            help="Interactive Mode, this will present a list "
+                "to make it possible to select mirrors you wish to use.")
 
-		group = parser.add_option_group("Main modes")
-		group.add_option(
-			"-i", "--interactive", action="store_true", default=False,
-			help="Interactive Mode, this will present a list "
-			"to make it possible to select mirrors you wish to use.")
+        group = parser.add_option_group("Other options")
+        group.add_option(
+            "-d", "--debug", action="store", type="int", dest="verbosity",
+            default=1, help="debug mode, pass in the debug level [1-9]")
+        group.add_option(
+            "-f", "--file", action="store", default='mirrorselect-test',
+            help="An alternate file to download for deep testing. "
+                    "Please choose the file carefully as to not abuse the system "
+                    "by selecting an overly large size file.  You must also "
+                    " use the -m, --md5 option.")
+        group.add_option(
+            "-o", "--output", action="store_true", default=False,
+            help="Output Only Mode, this is especially useful "
+                "when being used during installation, to redirect "
+                "output to a file other than %s" % config_path)
+        group.add_option(
+            "-q", "--quiet", action="store_const", const=0, dest="verbosity",
+            help="Quiet mode")
 
-		group = parser.add_option_group("Other options")
-		group.add_option(
-			"-d", "--debug", action="store", type="int", dest="verbosity",
-			default=1, help="debug mode, pass in the debug level [1-9]")
-		group.add_option(
-			"-f", "--file", action="store", default='mirrorselect-test',
-			help="An alternate file to download for deep testing. "
-				"Please choose the file carefully as to not abuse the system "
-				"by selecting an overly large size file.  You must also "
-				" use the -m, --md5 option.")
-		group.add_option(
-			"-o", "--output", action="store_true", default=False,
-			help="Output Only Mode, this is especially useful "
-			"when being used during installation, to redirect "
-			"output to a file other than %s" % config_path)
-		group.add_option(
-			"-q", "--quiet", action="store_const", const=0, dest="verbosity",
-			help="Quiet mode")
+        if len(argv) == 1:
+            parser.print_help()
+            sys.exit(1)
 
-		if len(argv) == 1:
-			parser.print_help()
-			sys.exit(1)
+        options, args = parser.parse_args(argv[1:])
 
-		options, args = parser.parse_args(argv[1:])
+        if (os.getuid() != rootuid) and not options.output:
+            self.output.print_err('Must be root to write to %s!\n' % config_path)
 
-		if (os.getuid() != rootuid) and not options.output:
-			self.output.print_err('Must be root to write to %s!\n' % config_path)
+        if args:
+            self.output.print_err('Unexpected arguments passed.')
 
-		if args:
-			self.output.print_err('Unexpected arguments passed.')
-
-		# return results
-		return options
+        # return results
+        return options
 
 
-	def get_available_features(self, options):
-		'''Returns a list of features suitable for consideration by a user
-		based on user input
+    def get_available_features(self, options):
+        '''Returns a list of features suitable for consideration by a user
+        based on user input
 
-		@param options: parser.parse_args() options instance
-		@rtype: list
-		'''
-		# self.output.write("using features:\n" % FEATURES_XML, 2)
-		features = [
-			('FooFeature', {'feature': 'foo', 'description': 'fooness'}),
-			('BarFeature', {'feature': 'bar', 'description': 'barness'}),
-		]
-		return features
+        @param options: parser.parse_args() options instance
+        @rtype: list
+        '''
+        # self.output.write("using features:\n" % FEATURES_XML, 2)
+        features = [
+            ('FooFeature', {'feature': 'foo', 'description': 'fooness'}),
+            ('BarFeature', {'feature': 'bar', 'description': 'barness'}),
+        ]
+        return features
 
-	def select_features(self, features, options):
-		'''Returns the list of selected host urls using
-		the options passed in interactive ncurses dialog
+    def select_features(self, features, options):
+        '''Returns the list of selected host urls using
+        the options passed in interactive ncurses dialog
 
-		@param hosts: list of features to choose from
-		@param options: parser.parse_args() options instance
-		@rtype: list
-		'''
-		selector = Interactive(features, options, self.output)
-		return selector.features
+        @param hosts: list of features to choose from
+        @param options: parser.parse_args() options instance
+        @rtype: list
+        '''
+        selector = Interactive(features, options, self.output)
+        return selector.features
 
-	def get_conf_path(self):
-		'''Checks for the existance of repos.conf or make.conf in /etc/portage/
-		Failing that it checks for it in /etc/
-		Failing in /etc/ it defaults to /etc/portage/make.conf
+    def get_conf_path(self):
+        '''Checks for the existance of repos.conf or make.conf in /etc/portage/
+        Failing that it checks for it in /etc/
+        Failing in /etc/ it defaults to /etc/portage/make.conf
 
-		@rtype: string
-		'''
-		return get_kernel_ng_conf_path(EPREFIX)
+        @rtype: string
+        '''
+        return get_kernel_ng_conf_path(EPREFIX)
 
-	def main(self, argv):
-		"""Lets Rock!
+    def main(self, argv):
+        """Lets Rock!
 
-		@param argv: list of command line arguments to parse
-		"""
-		config_path = self.get_conf_path()
-		options = self._parse_args(argv, config_path)
-		self.output.verbosity = options.verbosity
-		self.output.write("main(); config_path = %s\n" % config_path, 2)
+        @param argv: list of command line arguments to parse
+        """
+        config_path = self.get_conf_path()
+        options = self._parse_args(argv, config_path)
+        self.output.verbosity = options.verbosity
+        self.output.write("main(); config_path = %s\n" % config_path, 2)
 
-		# reset config_path to find repos.conf/gentoo.conf if it exists
-		config_path = self.get_conf_path()
-		self.output.write("main(); reset config_path = %s\n" % config_path, 2)
+        # reset config_path to find repos.conf/gentoo.conf if it exists
+        config_path = self.get_conf_path()
+        self.output.write("main(); reset config_path = %s\n" % config_path, 2)
 
-		features = self.get_available_features(options)
+        features = self.get_available_features(options)
 
-		pickedfeatures = self.select_features(features, options)
+        pickedfeatures = self.select_features(features, options)
 
-		if len(pickedfeatures):
-			self.change_config(pickedfeatures, options.output,
-				config_path)
-		else:
-			self.output.write("No results found. "
-				"Check your settings and re-run look-ma-no-sources.\n")
+        if len(pickedfeatures):
+            self.change_config(pickedfeatures, options.output,
+                config_path)
+        else:
+            self.output.write("No results found. "
+                "Check your settings and re-run look-ma-no-sources.\n")
