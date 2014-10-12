3e893aaf41c0af4647d871c263c6e63384dc7d91
Gregory M. Turner <gmt@be-evil.net>
2014-10-12 08:18:09 -0700

    selectors: fix whitespace and fix broken leftover variable name
    Signed-off-by: Gregory M. Turner <gmt@be-evil.net>

--- lookmanosources/selectors.py
+++ lookmanosources/selectors.py
@@ -33,46 +33,44 @@ from lookmanosources.output import encoder, get_encoding, decode_selection
 
 
 class Interactive(object):
-	"""Handles interactive features selection."""
+    """Handles interactive features selection."""
 
-	def __init__(self, features, options, output):
-		self.output = output
-		self.features = []
+    def __init__(self, features, options, output):
+        self.output = output
+        self.features = []
 
-		self.interactive(features, options)
-		self.output.write('Interactive.interactive(): self.features = %s\n'
-			% self.features, 2)
+        self.interactive(features, options)
+        self.output.write('Interactive.interactive(): self.features = %s\n'
+            % self.features, 2)
 
-		if not self.features or len(self.features[0]) == 0:
-			sys.exit(1)
+        if not self.features or len(self.features[0]) == 0:
+            sys.exit(1)
 
+    def interactive(self, features, options):
+       """
+        Some sort of interactive menu thingy.
+       """
+        dialog = ['dialog', '--separate-output', '--stdout', '--title',
+            '"Select kernel feature sets:"', '--checklist',
+           '"Please select your desired features:']
+        dialog.extend(['20', '110', '14'])
+        for (feature, args) in sorted(features, key = lambda x:
+            (x[1]['feature'], x[1]['description']) ):
+            dialog.extend(["%s" %feature,
+                "%s: %s" %(args['feature'], args['description']),
+                "OFF"])
+            dialog = [encoder(x, get_encoding(sys.stdout)) for x in dialog]
+            proc = subprocess.Popen( dialog,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        def interactive(self, features, options):
-		"""
-		Some sort of interactive menu thingy.
-		"""
-		
-                dialog = ['dialog', '--separate-output', '--stdout', '--title',
-                          '"Select kernel feature sets:"', '--checklist',
-		          '"Please select your desired features:']
-		dialog.extend(['20', '110', '14'])
-		for (feature, args) in sorted(features, key = lambda x:
-				(x[1]['feature'], x[1]['description']) ):
-			dialog.extend(["%s" %feature,
-				"%s: %s" %(args['feature'], args['description']),
-				 "OFF"])
-		dialog = [encoder(x, get_encoding(sys.stdout)) for x in dialog]
-		proc = subprocess.Popen( dialog,
-			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = proc.communicate()
 
-		out, err = proc.communicate()
+            self.features = out.splitlines()
 
-		self.features = out.splitlines()
-
-		if self.urls:
-			if hasattr(self.features[0], 'decode'):
-				self.features = decode_selection(
-					[x.decode('utf-8').rstrip() for x in self.features])
-			else:
-				self.features= decode_selection([x.rstrip() for x in self.features])
+            if self.features:
+                if hasattr(self.features[0], 'decode'):
+                    self.features = decode_selection(
+                        [x.decode('utf-8').rstrip() for x in self.features])
+                else:
+                    self.features= decode_selection([x.rstrip() for x in self.features])
 
